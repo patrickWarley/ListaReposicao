26d6516cd2087f5ed2b9d50b496f64541b8acb55
Patrick <patrickwarley@gmail.com>
2023-07-09 21:43:52 -0300

    access to the list of users created

--- src/App.jsx
+++ src/App.jsx
@@ -8,105 +8,116 @@ import axios from 'axios';
 import UserSelector from './assets/components/UserSelector';
 
 function App() {
-  const [currList, setCurrList] = useState(0);
-  const [lists, setLists] = useState(null);
-  const [showList, setShowList] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [categories, setCategories] = useState([]);
+	const [currList, setCurrList] = useState(0);
+	const [lists, setLists] = useState(null);
+	const [showList, setShowList] = useState(null);
+	const [selectedCategory, setSelectedCategory] = useState(null);
+	const [categories, setCategories] = useState([]);
+	const [users, setUsers] = useState([]);
 
-  const getListsServer = async () => {
-    try {
-      const request = await axios.get('/reposicao.json');
-      if (request.status === 200) {
-        setLists(request.data.lists);
-        setShowList(request.data.lists[0]);
-        setCategories(request.data.categories);
-      }
-    } catch (e) { console.log(e); }
-  }
+	const getListsServer = async () => {
+		try {
+			const request = await axios.get('/reposicao.json');
+			if (request.status === 200) {
+				setLists(request.data.lists);
+				setShowList(request.data.lists[0]);
+				setCategories(request.data.categories);
+			}
+		} catch (e) { console.log(e); }
+	}
 
-  const getListsLocal = () => {
-    setLists(reposicao.lists);
-    setShowList(reposicao.lists[0]);
-    setCategories(reposicao.categories);
-  }
+	const getUsers = async () => {
+		try {
+			const request = await axios.get('/user');
+			if (request.status === 200) setUsers(request.data);
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  useEffect(() => {
-    getListsServer();
-  }, []);
+	const getListsLocal = () => {
+		setLists(reposicao.lists);
+		setShowList(reposicao.lists[0]);
+		setCategories(reposicao.categories);
+	}
 
-  useEffect(() => {
-    filter();
-  }, [currList, selectedCategory]);
+	useEffect(() => {
+		getListsServer();
+		getUsers();
+	}, []);
 
-  const proximaLista = (incr) => {
-    let nextList = currList + incr;
-    nextList === lists.length ? nextList = 0 : nextList < 0 ? nextList = lists.length - 1 : nextList;
-    return setCurrList(nextList);
-  }
+	useEffect(() => {
+		filter();
+	}, [currList, selectedCategory]);
 
-  const orderByEndereco = () => {
-    console.log("Need to implement this!");
-  }
+	const proximaLista = (incr) => {
+		let nextList = currList + incr;
+		nextList === lists.length ? nextList = 0 : nextList < 0 ? nextList = lists.length - 1 : nextList;
+		return setCurrList(nextList);
+	}
 
-  const orderBySku = () => {
-    console.log("Need to implement this!");
-  }
+	const orderByEndereco = () => {
+		console.log("Need to implement this!");
+	}
 
-  const filter = () => {
+	const orderBySku = () => {
+		console.log("Need to implement this!");
+	}
 
-    if (showList === null) return;
+	const filter = () => {
 
-    //if selectedCategory is null just reset the listcenter
-    if (selectedCategory === null) return setShowList(lists[currList]);
+		if (showList === null) return;
 
-    //each row is a different category
-    var result = lists[currList].rows.filter(category => selectedCategory.indexOf(category[0]) !== -1);
+		//if selectedCategory is null just reset the listcenter
+		if (selectedCategory === null) return setShowList(lists[currList]);
 
-    if (result.length !== 0) return setShowList({ "name": lists[currList].name, "rows": result });
+		//each row is a different category
+		var result = lists[currList].rows.filter(category => selectedCategory.indexOf(category[0]) !== -1);
 
-    setShowList(lists[currList]);
-  }
+		if (result.length !== 0) return setShowList({ "name": lists[currList].name, "rows": result });
 
-  const setUser = (user) => {
+		setShowList(lists[currList]);
+	}
 
-    console.log(user)
-    //const request = axios.get('/user.json');
-    //get the users info
-    //find the info of the user that interests me
-    //set everything
-  }
+	const setUser = (user) => {
 
-  return (
-    <div className='min-h-screen w-screen dark:bg-slate-800 flex flex-col items-center'>
-      <OffCanvas title={'Menu'}>
-        <Menu
-          callback={(categories) => setSelectedCategory(categories)}
-          categories={categories}
-          initialValues={selectedCategory}
-        />
-      </OffCanvas>
-      <div className=' w-full contianer justify-end flex'>
-        <UserSelector
-          users={['Veronica', 'Vinicius', 'Morcego', 'Josimar', 'Patrick']}
-          onSelect={(user) => setUser(user)}
-        />
-        <Toggle />
-        <button onClick={() => proximaLista(-1)} className=' p-3 m-3 bg-green-400 border drop-shadow'>Anterior</button>
-        <button onClick={() => proximaLista(1)} className=' p-3 m-3 bg-green-400 border drop-shadow'>Proxima</button>
-      </div>
-      {
-        lists !== null &&
-        <div className='w-full flex flex-col items-center justify-center'>
-          <h1 className='dark:text-white font-extrabold'>{showList?.name}</h1>
-          {
-            showList?.rows.map(category =>
-              < List key={category[0]} list={category[1]} orderByEndereco={orderByEndereco} orderBySku={orderBySku} />)
-          }
-        </div>
-      }
-    </div >
-  )
+		console.log(user)
+		//const request = axios.get('/user.json');
+		//get the users info
+		//find the info of the user that interests me
+		//set everything
+	}
+
+	return (
+		<div className='min-h-screen w-screen dark:bg-slate-800 flex flex-col items-center'>
+			<OffCanvas title={'Menu'}>
+				<Menu
+					callback={(categories) => setSelectedCategory(categories)}
+					categories={categories}
+					initialValues={selectedCategory}
+				/>
+			</OffCanvas>
+			<div className=' w-full contianer justify-end flex'>
+				<UserSelector
+					users={users}
+					onSelect={(user) => setUser(user)}
+				/>
+				<Toggle />
+				<button onClick={() => proximaLista(-1)} className=' p-3 m-3 bg-green-400 border drop-shadow'>Anterior</button>
+				<button onClick={() => proximaLista(1)} className=' p-3 m-3 bg-green-400 border drop-shadow'>Proxima</button>
+			</div>
+			{
+				lists !== null &&
+				<div className='w-full flex flex-col items-center justify-center'>
+					<h1 className='dark:text-white font-extrabold'>{showList?.name}</h1>
+					{
+						showList?.rows.map(category =>
+							< List key={category[0]} list={category[1]} orderByEndereco={orderByEndereco} orderBySku={orderBySku} />)
+					}
+				</div>
+			}
+		</div >
+	)
 }
 
 export default App
